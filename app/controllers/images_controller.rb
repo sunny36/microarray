395b628dc60dee1ac158b8af3698dba9ff14b699
Somchok Sakjiraphong <s@sunny.in.th>
2011-10-13 03:02:32 +0700

    Show experiment name

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,6 +2,7 @@ class ImagesController < ApplicationController
   respond_to :html, :json
   def index
     @images = Image.for_experiment(params[:experiment_id]).only_templates
+    @experiment = Experiment.find(params[:experiment_id])
     respond_with(@images)
   end
 end
