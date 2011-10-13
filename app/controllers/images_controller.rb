7ea0e7aacbdc8aa978af0e9e0267f0da957f7827
Somchok Sakjiraphong <s@sunny.in.th>
2011-10-14 06:56:25 +0700

    Temporary fix to work with postgres on heroku

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,8 @@
 class ImagesController < ApplicationController
   respond_to :html, :json
   def index
-    @images = Image.for_experiment(params[:experiment_id]).only_templates
+    #@images = Image.for_experiment(params[:experiment_id]).only_templates
+    @images = Image.where(params[:experiment_id]).where(is_template: true)
     @experiment = Experiment.find(params[:experiment_id])
     respond_with(@images)
   end
